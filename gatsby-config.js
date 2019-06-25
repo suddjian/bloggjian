a45341f1096222f1db02d944b13aa3a7ff9f6678
David Aaron Suddjian <aasuddjian@gmail.com>
2019-06-24 20:20:59 -0700

    changing icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Suddjian Blog`,
+        short_name: `Suddjian Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/blog-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
