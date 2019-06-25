359b65c1d1dd63fa26cfb4bb13559cc006008077
David Aaron Suddjian <aasuddjian@gmail.com>
2019-06-24 21:07:08 -0700

    figured out how to increase image quality

--- src/components/bio.js
+++ src/components/bio.js
@@ -16,7 +16,7 @@ const Bio = () => {
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic-2.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 50, height: 50, quality: 100) {
             ...GatsbyImageSharpFixed
           }
         }
