e2323f545213aeef5c46f4be84fadd756c2f4585
David Aaron Suddjian <aasuddjian@gmail.com>
2019-06-24 20:28:57 -0700

    change blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Suddjian Blog`,
+    title: `Suddjian, the Blog`,
     author: `David Aaron Suddjian`,
     description: `Thoughts on programming, relationships, and life`,
     siteUrl: `https://blog.suddjian.com`,
