5bda549a6fa7f441e3f53923e0fb434696070f0a
David Aaron Suddjian <aasuddjian@gmail.com>
2019-07-31 00:07:17 -0700

    comma is better lol

--- src/components/bio.js
+++ src/components/bio.js
@@ -57,7 +57,7 @@ const Bio = () => {
         }}
       />
       <p>
-        Personal blog of <strong>{author}</strong>: a programmer from Santa Cruz building useful things for money and useless things for fun.
+        Personal blog of <strong>{author}</strong>, a programmer from Santa Cruz building useful things for money and useless things for fun.
         <br />
         Currently <a href={`mailto:${authorEmail}`}>looking for work</a> at an awesome company!
       </p>
